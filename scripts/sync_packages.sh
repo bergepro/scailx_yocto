a0806f7185b17905fb2426b50d3ce56c2169002e
Kobus Goosen <kgoosen@videologyinc.com>
2024-11-21 16:51:03 +0100

    tiny fixes to settings, unused files, scripts.

--- scripts/sync_packages.sh
+++ scripts/sync_packages.sh
@@ -2,6 +2,5 @@
 
 # needs to be rnu from build folder, in yocto context.
 
-bitbake package-index
 # upload packages for installable arch's 
-rclone sync deploy/ipk/ scailx-storage:scailx-ppa/scailx/6.6-scarthgap/deploy/ipk/ --progress --transfers 64 --checkers 64 --checksum
+bitbake package-index && rclone sync deploy/ipk/ scailx-storage:scailx-ppa/scailx/6.6-scarthgap/deploy/ipk/ --progress --transfers 64 --checkers 64 --checksum
